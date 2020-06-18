b91a2cbc40a2c853b9578fb63d200302070940c2
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-06-18 17:21:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -4,40 +4,45 @@ def welcome
 end
 
 def deal_card
-  # code #deal_card here
-  puts rand(11)
+  rand(1..11)
 end
 
 def display_card_total(card_total)
-  # code #display_card_total here
-   puts "Your cards add up to #{card_total}"
+  puts "Your cards add up to #{card_total}"
 end
 
+
+##########
+# rounds #
+##########
+
+def welcome
+  puts "Welcome to the Blackjack Table"
+end
+
+
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
-def get_user_input
-  # code #get_user_input here
-  user_input= gets.chomp
-end
-
-def end_game(card_total)
-  # code #end_game here
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
-
-end
 
 def initial_round
-  # code #initial_round here
   first_round = deal_card + deal_card
   display_card_total(first_round)
   return first_round
 end
 
-def hit?(current_card)
-  # code hit? here
+def get_user_input
+  gets.chomp.strip
+end
+
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+end
+
+
+
+def hit?(card_total)
   prompt_user
   user_input = get_user_input
   if user_input == "h"
@@ -49,25 +54,21 @@ def hit?(current_card)
   end
 end
 
-end
-
 def invalid_command
-  # code invalid_command here
-  puts "Sorry, not a valid command."
+  puts "Sorry, not a valid command"
   get_user_input
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+##########
+# runner #
+##########
 
 def runner
-  # code runner here
   welcome
- card_total = initial_round
- until card_total > 21
-   card_total = hit?(card_total)
-   display_card_total(card_total)
- end
- end_game(card_total)
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
